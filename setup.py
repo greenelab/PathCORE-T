0de6aadb1a1e2e52e46127eea672407789ee6554
Kathy Chen <kchen2013@gmail.com>
2017-08-21 21:36:31 -0400

    Update setup.py with latest version 1.0.2
    Hotfix to update the major.minor.patch release. Forgot to do so during the last README update to this repository.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="PathCORE",
-    version="1.0.1",
+    version="1.0.2",
     packages=["pathcore"],
     include_package_data=True,
     license="BSD-3-Clause",
